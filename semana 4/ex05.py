0a4e451ecdf1ecc4f1037c51d1b960b7ed0ca71c
Mizaelknirsch <knirschofc@gmail.com>
2025-03-27 17:38:43 -0300

    Create ex05.py

---
+++ semana 4/ex05.py
@@ -0,0 +1,8 @@
+altura = float(input("DIGITE SUA ALTURA: "))
+sexo = str(input("digite seu sexo: "))
+if sexo == "homem":
+    sexo = (72.7* altura) -58
+    print(peso: "sexo")
+if sexo == "feminino":
+    sexo = (62,1 * altura) - 44,7
+    print("peso: , sexo")
