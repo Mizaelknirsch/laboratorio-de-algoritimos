2109d903517838d5681a36759291220b8742f5fb
Mizaelknirsch <knirschofc@gmail.com>
2025-04-03 23:36:56 -0300

    Create Ex06.py

---
+++ samana05/Ex06.py
@@ -0,0 +1,18 @@
+cart = str(input("qual sua CNH: "))
+if cart == "A":
+    print("Motos e ticiclos.")
+
+elif cart == "B":
+    print("Carros de passeios.")
+
+elif cart == "C":
+    print("Veiculos de carga maxima de 3.5 toneladas.")
+
+elif cart == "D":
+    print("Veiclos com mais de 8 passageiros")
+
+elif cart == "E":
+    print("veiculos com mais de 6 toneladas.")
+
+else:
+    print("CNH não existe!!")
