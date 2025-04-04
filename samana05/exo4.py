4432f2ec44cd555e6c77f8cdafe9467f125a104a
Mizaelknirsch <knirschofc@gmail.com>
2025-04-03 23:35:31 -0300

    Create exo4.py

---
+++ samana05/exo4.py
@@ -0,0 +1,19 @@
+print("escolha as seguintes respostas: "
+"  ,A"
+"  ,B"
+"  ,C"
+"  ,D")
+
+
+resp = str(input("digite uma resposta:  "))
+if resp == "A":
+    print('RESPOSTA ERRADA!')
+
+elif resp == "B":
+    print("RESPOSTA CERTA!")
+
+elif resp == "C":
+    print('RESPOSTA ERRADA!')
+
+elif resp == "D":
+    print('RESPOSTA ERRADA!')
