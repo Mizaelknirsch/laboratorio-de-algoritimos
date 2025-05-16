c5752a526983a893b9e51265f8429c29547ed7c3
Mizaelknirsch <knirschofc@gmail.com>
2025-05-15 22:01:09 -0300

    Create ex06.py

---
+++ semana10/ex06.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    numero = int(input(f"Digite o {i+1}º valor: "))
+    while numero in valores:
+        print("Valor repetido! Digite um valor distinto.")
+        numero = int(input(f"Digite o {i+1}º valor novamente: "))
+    valores.append(numero)
+
+
+print("\nNúmeros pares e suas posições:")
+for i in range(10):
+    if valores[i] % 2 == 0:
+        print(f"Valor {valores[i]} na posição {i}")
