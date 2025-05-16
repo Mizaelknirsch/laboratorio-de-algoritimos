79bd252091ba6d361e6b350a9227dfcca8e77054
Mizaelknirsch <knirschofc@gmail.com>
2025-05-15 21:46:02 -0300

    Update ex04.py

--- semana10/ex04.py
+++ semana10/ex04.py
@@ -1,6 +1,6 @@
 vetor_A = []
 
-# Ler 10 valores distintos
+
 while len(vetor_A) < 10:
     valor = int(input(f"Digite o valor {len(vetor_A)+1}: "))
     if valor in vetor_A:
@@ -10,7 +10,7 @@ while len(vetor_A) < 10:
 
 print(f"Vetor A: {vetor_A}")
 
-# Criar vetor B com os valores de A ao contrário
+
 vetor_B = vetor_A[::-1]
 
 print(f"Vetor B (ordem inversa): {vetor_B}")
