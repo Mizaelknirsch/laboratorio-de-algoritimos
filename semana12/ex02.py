2ab264d7a2b5a0862afc9e311af3a0774bd99142
Mizaelknirsch <knirschofc@gmail.com>
2025-05-29 17:23:27 -0300

    Create ex02.py

---
+++ semana12/ex02.py
@@ -0,0 +1,17 @@
+def valor_2():
+    numero =float(input("Digite um valor :"))
+    dobro = numero * 2
+    print(dobro)
+    
+    
+def valor_3():
+    numero = float(input("Digite um valor :"))
+    triplo  = numero * 3
+    print("valor com o triplo: ", triplo)    
+    
+def main():
+    valor_2()
+    valor_3()  
+
+
+main()
