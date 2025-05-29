5a421e50e49934ccee35bd83a77865cea869f5d3
Mizaelknirsch <knirschofc@gmail.com>
2025-05-29 17:29:26 -0300

    Update ex07.py

--- semana12/ex07.py
+++ semana12/ex07.py
@@ -1,3 +1,4 @@
+
 def converter_para_12h(hora, minuto):
     if hora == 0:
         return 12, minuto, "A.M."
@@ -8,6 +9,14 @@ def converter_para_12h(hora, minuto):
     else:
         return hora - 12, minuto, "P.M."
 
-
 def exibir_horario(h12, min, periodo):
-    print(f"Horário convertido: {
+    print(f"Horário convertido: {h12}:{min:02d} {periodo}")
+
+hora = int(input("Digite a hora (0-23): "))
+minuto = int(input("Digite os minutos (0-59): "))
+
+if 0 <= hora <= 23 and 0 <= minuto <= 59:
+    h12, min_conv, periodo = converter_para_12h(hora, minuto)
+    exibir_horario(h12, min_conv, periodo)
+else:
+    print("Hora ou minuto inválido.")
